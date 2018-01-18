ba0655edee01b839b782a954ffca4adb4004406c
alici <alici@PC_Alicia_dev>
2018-01-18 12:49:05 +0100

    ajout constructeurs pour plus de polyvalence sur les instruments

--- src/pckgMesGammes/NoteCellRenderer.java
+++ src/pckgMesGammes/NoteCellRenderer.java
@@ -5,7 +5,6 @@
  */
 package pckgMesGammes;
 
-import java.awt.Color;
 import java.awt.Component;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
@@ -19,11 +18,32 @@ import javax.swing.table.DefaultTableCellRenderer;
       * tableau de NoteDrawPanel de 2 lignes (français, anglais) ou on retient les dessins de note pour une colonne
       * pour l'affichage des notes d'une gamme
       */
-     private NoteDrawPanel[] ndp=new NoteDrawPanel[2];
+     private NoteDrawPanel[] ndp;
      
+     /**
+      * constructeur utilise pour le tableau affichant la gamme cree un tableau de deux entrees 
+      * Note graphique (dessin de rond avec texte) en français et anglais
+      * @param note Note
+      * @param x int
+      */
      public NoteCellRenderer(Note note, int x){
+         ndp=new NoteDrawPanel[2];
          this.ndp[0]=new NoteDrawPanel(note.get_couleur(), note.get_nom_fr(), x, 1);         
          this.ndp[1]=new NoteDrawPanel(note.get_couleur(), note.get_nom_e(), x, 1); 
+     }
+     
+      /**
+      * constructeur utilise pour le tableau affichant les notes sur un instrument
+      * le tableau de notes contient toutes les notes d'une colonne
+      * @param notes Note[]
+      * @param x int
+      * @param taille
+      */
+     public NoteCellRenderer(Note[] notes, int x, int taille){
+         ndp=new NoteDrawPanel[taille];
+         for(int i=0; i<taille; i++){
+             this.ndp[i]=new NoteDrawPanel(notes[i].get_couleur(), notes[i].get_nom_fr(), x, 1);  
+         }
      }
      
       public NoteDrawPanel[] getNdp() {
