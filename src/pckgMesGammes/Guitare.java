0302244e4bc06f0fca0b455757468750654ee3f9
alici <alici@PC_Alicia_dev>
2019-01-30 16:08:50 +0100

    Correction affichage des notes sur le manche

--- src/pckgMesGammes/Guitare.java
+++ src/pckgMesGammes/Guitare.java
@@ -3,6 +3,7 @@ package pckgMesGammes;
 import java.util.LinkedHashMap;
 
 class Guitare extends Instrument {
+
     /**
      * nombre de fretes sur guitare
      */
@@ -14,23 +15,23 @@ class Guitare extends Instrument {
     /**
      * constante qui donne la hauteur d'une case dans le tableau
      */
-    public static final int GUITARE_CELL_HEIGHT=30;
+    public static final int GUITARE_CELL_HEIGHT = 30;
     /**
      * constante qui donne la largeur d'une case dans le tableau
      */
-    public static final int GUITARE_CELL_WIDTH=75;
-   
-    
+    public static final int GUITARE_CELL_WIDTH = 75;
+
     /**
-     *constante qui definit les images correspondant a chaque case de la guitare
+     * constante qui definit les images correspondant a chaque case de la
+     * guitare
      */
-    public static final String[][] GUITARE_IMAGES={
-    {TETE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE},
-    {TETE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CLOU},
-    {TETE, CORDE, CORDE, DEMI_CLOU_DOWN, CORDE, DEMI_CLOU_DOWN, CORDE, DEMI_CLOU_DOWN, CORDE, DEMI_CLOU_DOWN, CORDE, CORDE, CORDE},
-    {TETE, CORDE, CORDE, DEMI_CLOU_UP, CORDE, DEMI_CLOU_UP, CORDE, DEMI_CLOU_UP, CORDE, DEMI_CLOU_UP, CORDE, CORDE, CORDE},
-    {TETE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CLOU},
-    {TETE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE}};
+    public static final String[][] GUITARE_IMAGES = {
+        {TETE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE},
+        {TETE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CLOU},
+        {TETE, CORDE, CORDE, DEMI_CLOU_DOWN, CORDE, DEMI_CLOU_DOWN, CORDE, DEMI_CLOU_DOWN, CORDE, DEMI_CLOU_DOWN, CORDE, CORDE, CORDE},
+        {TETE, CORDE, CORDE, DEMI_CLOU_UP, CORDE, DEMI_CLOU_UP, CORDE, DEMI_CLOU_UP, CORDE, DEMI_CLOU_UP, CORDE, CORDE, CORDE},
+        {TETE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CLOU},
+        {TETE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE, CORDE}};
     /**
      * tableau avec nonm et notes des differents Tunning (accordage guitare)
      */
@@ -67,6 +68,7 @@ class Guitare extends Instrument {
     /**
      * constructeur de base d'une guitare, construit le tableau contenant toutes
      * les notes du manche en fonction du tunning passe en parametre
+     *
      * @param String tunning
      */
     public Guitare(String tunning) {
@@ -89,7 +91,6 @@ class Guitare extends Instrument {
         this.ajuste_note_instrum(gamme);
         this.gamme_on_instrum(gamme);
         //copier des notes de notes_instrum sur gamme_instrum en eliminant celles qui ne se trouvent pas sur la gamme
-        
 
     }
 
@@ -100,10 +101,16 @@ class Guitare extends Instrument {
     public void init_instrum(String tunning) {
         this.set_tunning();
         //on se base sur le tunning standard
-        String[] tunning_std = new StringBuilder(this.tunning.get(tunning)).toString().split(" ");
+        String[] tunning_std = this.tunning.get(tunning).split(" ");
+        //inverser le tableau
+        for (int i = 0; i < tunning_std.length / 2; i++) {
+            String temp = tunning_std[i];
+            tunning_std[i] = tunning_std[tunning_std.length - i - 1];
+            tunning_std[tunning_std.length - i - 1] = temp;
+        }
         //on cree un tableau de Note de longueur NB_FRETES_GUITARE
         //parcours à l'envers pour afficher les notes dans le bon ordre
-        for (int l = this.ligne_instrum-1; l >=0 ; l--) {
+        for (int l = this.ligne_instrum - 1; l >= 0; l--) {
             //on cree les notes pour une corde en fonction des notes du tunning
             Note base = new Note(tunning_std[l]);
             int i = 0;
@@ -117,17 +124,15 @@ class Guitare extends Instrument {
                 i++;
             } while (i < this.taille_instrum);
         }
-           this.instrument_cell_width=GUITARE_CELL_WIDTH;
-        this.instrument_cell_height=GUITARE_CELL_HEIGHT;
-        this.instrument_images=GUITARE_IMAGES;
+        this.instrument_cell_width = GUITARE_CELL_WIDTH;
+        this.instrument_cell_height = GUITARE_CELL_HEIGHT;
+        this.instrument_images = GUITARE_IMAGES;
     }
 
     @Override
     public void set_tunning() {
-        this.tunning=TUNNING_GUITARE;
+        this.tunning = TUNNING_GUITARE;
     }
-   
-   
 
     //    public get_positions_accord(Accord accord) {
 //    }
@@ -139,8 +144,8 @@ class Guitare extends Instrument {
     public static void main(String args[]) {
 //        Gamme g = new Gamme("G", "MAJEUR");
 //        Piano p = new Piano(g);
-        Guitare g = new Guitare(new Gamme("G", "MAJEUR"),"HALF_STEP_DOWN");
-        
+        Guitare g = new Guitare(new Gamme("G", "MAJEUR"), "HALF_STEP_DOWN");
+
         System.out.println(g);
         System.out.println(g.getNoteGammeAtPos(4, 1));
     }
